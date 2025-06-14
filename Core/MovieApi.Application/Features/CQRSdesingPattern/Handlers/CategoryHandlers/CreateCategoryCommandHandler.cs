edd437b0c764e1c6f6cd482c91695d5eafdd9dd7
AliBaydur <59739580+AliBaydur@github.com>
2025-06-14 23:39:15 +0300

    CORSDesingPattern

---
+++ Core/MovieApi.Application/Features/CQRSdesingPattern/Handlers/CategoryHandlers/CreateCategoryCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MovieApi.Application.Features.CQRSdesingPattern.Commands.CategoryCommands;
+using MovieApi.Persistence.Context;
+using MovieAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSdesingPattern.Handlers.CategoryHandlers
+{
+    public class CreateCategoryCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public CreateCategoryCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async void Handle(CreateCategoryCommand command)
+        {
+            _context.Categories.Add(new Category
+            {
+                CategoryName = command.CategoryName,
+            });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
